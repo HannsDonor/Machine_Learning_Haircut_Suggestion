a0d3959e8cc76c62de282369495a4e9be2d8fcd5
HannsDonor <167281350+HannsDonor@users.noreply.github.com>
2025-11-11 03:13:47 +0800

    Update start.py

--- start.py
+++ start.py
@@ -3,6 +3,7 @@ import uvicorn
 from app import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7860))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
+
 

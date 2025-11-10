3f55802262529e51f119073865691741e09f82ab
HannsDonor <167281350+HannsDonor@users.noreply.github.com>
2025-11-10 15:44:35 +0800

    Add files via upload

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
-# start.py
-from app import app
+import os
 import uvicorn
+from app import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
